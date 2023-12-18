ff32a5dbe8f8c4a0407fd605b8f40794760b9152
Paul <paul.niggli25@gmail.com>
2023-12-18 14:14:52 +0100

    login + register

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\LoginType;
+use App\Form\RegisterType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class SecurityController extends AbstractController
+{
+
+    private $entityManager;
+
+    // Injection de dépendance par le constructeur
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
+    {
+        $form = $this->createForm(LoginType::class);
+        $form->handleRequest($request);
+        $userrepo = $this->entityManager->getRepository(User::class);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+            $mail = $formData->getEmail();
+            $user = $userrepo->findOneBy(["email"=>$mail]);
+            if(md5($formData->getPassword()) == $user->getPassword()){
+                if ($user->getRoles() == ['ROLE_ADMIN', 'ROLE_USER']){
+                    return $this->redirectToRoute('app_admin');
+                }else{
+                    return $this->redirectToRoute('app_index');
+                }
+
+            }
+        }
+        // render login form
+        return $this->render('security/login.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        // laisser vide
+    }
+
+
+    #[Route('/register', name: 'app_register', methods: ['GET', 'POST'])]
+    public function register(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(md5($user->getPassword()));
+            $user->setRoles(["ROLE_USER"]);
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
