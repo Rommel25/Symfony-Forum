1844019c49be0fcb75487f6c8bf93fa54f90dfaa
Paul <paul.niggli25@gmail.com>
2023-12-18 18:58:00 +0100

    auth complete avec droit et session + page profile

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use http\Client\Request;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
 
 class TestController extends AbstractController
 {
 
     #[Route('/index', name: 'app_index', methods: ['GET', 'POST'])]
-    public function index(): Response
+    #[IsGranted('ROLE_ADMIN')]
+    public function index(Security $security): Response
     {
+
+        $user = $security->getUser();
         return $this->render('general/index.html.twig');
     }
 
