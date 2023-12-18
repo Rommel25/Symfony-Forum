c95da75a86c05cf03e6b264b9b2ebb0ec6921217
antoine-falgiglio <antoine.falgiglio@bedrockstreaming.com>
2023-12-18 16:58:35 +0100

    get atelier from bdd

--- src/Controller/ListeAteliersController.php
+++ src/Controller/ListeAteliersController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Atelier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ListeAteliersController extends AbstractController
 {
     #[Route('/liste/ateliers', name: 'app_liste_ateliers')]
     public function index(): Response
     {
+        $userrepo = $this->entityManager->getRepository(Atelier::class);
+        $Atelier = $userrepo->findAll();
+        dd($Atelier);
         return $this->render('liste_ateliers/index.html.twig', [
             'controller_name' => 'ListeAteliersController',
         ]);
     }
+    private $entityManager;
+
+    // Injection de dépendance par le constructeur
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 }
+
+
+  
