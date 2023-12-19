70e5f516c0d0b17d9d1f42ebed9dfd910bf22ba2
mateocarciu <carciu98@gmail.com>
2023-12-19 10:37:40 +0100

    fix

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,7 +14,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -26,7 +25,7 @@ class SecurityController extends AbstractController
     {
         $this->entityManager = $entityManager;
     }
-
+    //Login fonctionne -> User vont vers index ou profile et admin va vers
     #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
     public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
@@ -38,15 +37,17 @@ class SecurityController extends AbstractController
             $formData = $form->getData();
             $mail = $formData->getEmail();
             $user = $userrepo->findOneBy(["email"=>$mail]);
+//            dd(md5($formData->getPassword()), $user->getPassword());
             if(md5($formData->getPassword()) == $user->getPassword()){
-                $token = new UsernamePasswordToken($user, "firewall", ["ROLE_USER"], $user->getRoles());
-                $this->container->get('security.token_storage')->setToken($token);
 
-//                $this->container->get('session')->set('_security_main', serialize($token));
+                if ($user->getRoles() == ['ROLE_ADMIN']){
 
-                if ($user->getRoles() == ['ROLE_ADMIN', 'ROLE_USER']){
-                    return $this->redirectToRoute('app_admin');
+                    $token = new UsernamePasswordToken($user, "firewall", ["ROLE_ADMIN"], $user->getRoles());
+                    $this->container->get('security.token_storage')->setToken($token);
+                    return $this->redirectToRoute('app_index', [$user]);
                 }else{
+                    $token = new UsernamePasswordToken($user, "firewall", ["ROLE_USER"], $user->getRoles());
+                    $this->container->get('security.token_storage')->setToken($token);
                     return $this->redirectToRoute('app_index');
                 }
 
@@ -66,7 +67,7 @@ class SecurityController extends AbstractController
         // laisser vide
     }
 
-
+    //Création de compte
     #[Route('/register', name: 'app_register', methods: ['GET', 'POST'])]
     public function register(Request $request): Response
     {
