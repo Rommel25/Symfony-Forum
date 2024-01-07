99a7aece8a429a68a8e6a4e90f18e9bddf4d2128
mateocarciu <carciu98@gmail.com>
2024-01-07 15:44:24 +0100

    ateliers show + details

--- src/Controller/AtelierController.php
+++ src/Controller/AtelierController.php
@@ -1,7 +1,11 @@
 <?php
 
+// AtelierController.php
+
 namespace App\Controller;
 
+use App\Entity\Atelier;
+use App\Repository\AtelierRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class AtelierController extends AbstractController
 {
     #[Route('/atelier', name: 'app_atelier')]
-    public function index(): Response
+    public function index(AtelierRepository $atelierRepository): Response
     {
+        // Fetch all ateliers from the repository
+        $ateliers = $atelierRepository->findAll();
+
         return $this->render('atelier/index.html.twig', [
-            'controller_name' => 'AtelierController',
+            'ateliers' => $ateliers,
         ]);
     }
 }
+
